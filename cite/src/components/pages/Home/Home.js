15fdc863afbf94823cc7b89f6484deb198a5f57a
amicxrly <krasavdhik00@gmail.com>
2024-02-27 20:26:24 +0300

    refactor: deleted list from Home.js

--- cite/src/components/pages/Home/Home.js
+++ cite/src/components/pages/Home/Home.js
@@ -1,23 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/aboutUs">О нас</Link>
-        </li>
-        <li>
-          <Link to="/services">Услуги</Link>
-        </li>
-        <li>
-          <Link to="/portfolio">Портфолио</Link>
-        </li>
-        <li>
-          <Link to="/contacts">Контакты</Link>
-        </li>
-      </ul>
-    </nav>
+    <h1>Главная страница</h1>
   );
 }
