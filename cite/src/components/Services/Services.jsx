dcd076e808fb6b8d7eaece1a7af6c915a457d6b6
amicxrly <krasavdhik00@gmail.com>
2024-02-27 23:33:11 +0300

    feat: add style for services title

---
+++ cite/src/components/Services/Services.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './services.css';
+
+function Services() {
+  return (
+    <div className="services">
+      <div className="container1">
+        <div className="textBlock">
+          <div className="texBlock_element">НАШИ УСЛУГИ</div>
+          <div className="texBlock_element2">
+            Какой нибудь отличный тейк о том какие
+            мы специалисты и воплотим все ваши мечты
+          </div>
+        </div>
+        <div className="cards" />
+      </div>
+    </div>
+  );
+}
+
+export default Services;
--- cite/src/components/app/App.js
+++ cite/src/components/app/App.js
@@ -1,9 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import Header from '../header/Header';
+import Services from '../Services/Services';
 
 function App() {
-  return (<Header />);
+  return (
+    <div>
+      <Header />
+      <Services />
+    </div>
+  );
 }
 
 export default App;
