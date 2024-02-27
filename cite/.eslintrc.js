8335d3c44dbe16f32fc8eac3e0bb13d41fe962c0
ikvankan <ikvankan@gmail.com>
2024-02-27 18:54:19 +0300

    feet: add card

--- cite/.eslintrc.js
+++ cite/.eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
   },
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'linebreak-style': 0,
   },
 
 };
--- cite/src/components/app/App.js
+++ cite/src/components/app/App.js
@@ -1,9 +1,16 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import Header from '../header/Header';
+import Card from '../card/card';
 
 function App() {
-  return (<Header />);
+  return (
+    <>
+      <Header />
+      <Card />
+    </>
+
+  );
 }
 
 export default App;
