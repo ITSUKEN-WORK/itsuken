bcc88d14197fcaa76951603d4c129d3c1d9ffe12
Fokstal <klabu2016@mail.ru>
2024-02-28 12:10:07 +0300

    style: add .gitignore file (Wed, 28.02.24 12:10 PM)

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f9ce8fe2e5b8302c51896c6650d0bc9e52ea351")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9941dd710a1aa2d11cc3f6ae600e2b0871c0e442")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
