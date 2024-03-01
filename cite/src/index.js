c46b63104a4c8a40822e2de1d0680b1f9a7f8707
NikLeshkov <leshkov06@mail.ru>
2024-03-01 09:38:21 +0300

    refactor: refactor code for app and index

--- cite/src/index.js
+++ cite/src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
-import FormFeedBack from './components/formFeedBack/FormFeedBack';
-import Portfolio from './components/portfolio/Portfolio';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <FormFeedBack />
-    <Portfolio />
   </React.StrictMode>,
 );
