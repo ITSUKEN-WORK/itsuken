4180011d9819b033ffc5d0d16f0abbe738080f8c
amicxrly <krasavdhik00@gmail.com>
2024-03-04 10:13:35 +0300

    fix: fixed pading in services

--- cite/src/index.js
+++ cite/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
+import Services from './components/Services/Services';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Services />
   </React.StrictMode>,
 );
