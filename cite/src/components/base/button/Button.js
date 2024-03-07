789163732af180f69a807f855a35a2d98a246f6c
Fokstal <klabu2016@mail.ru>
2024-03-07 10:17:04 +0300

    feat: add AJAX request for send email to API (Thu, 07.03.24 10:17 AM)

--- cite/src/components/base/button/Button.js
+++ cite/src/components/base/button/Button.js
@@ -3,8 +3,8 @@ import React from 'react';
 import './button.css';
 
 // eslint-disable-next-line react/prop-types
-function Button({ children, size = 'md', variant = 'filled', width = 'sm' }) {
-  return (<button className="button" type="button" data-size={size} data-variant={variant} data-width={width}>{children}</button>);
+function Button({ children, size = 'md', variant = 'filled', width = 'sm', onClick = null }) {
+  return (<button className="button" type="button" data-size={size} data-variant={variant} data-width={width} onClick={() => { if (onClick) onClick() }}>{children}</button>);
 }
 
 export default Button;
--- cite/src/components/formFeedBack/FormFeedBack.jsx
+++ cite/src/components/formFeedBack/FormFeedBack.jsx
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './formFeedBack.css';
 import Button from '../base/button/Button';
 import ContactButtons from '../contactButtons/ContactButtons';
 
 function FormFeedBack() {
+
+  const [name, setName] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [message, setMessage] = useState(null);
+
+  const sendToServerFeedback = () => {
+    fetch("http://localhost:5216/email/send-to-server", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        "name": name,
+        "email": email,
+        "message": message,
+      })
+    })
+      .then(resp => resp.text())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
   return (
     <form className="formFeedBack forma">
-      <input type="text" placeholder="Имя" className="textBox" />
-      <input type="text" placeholder="E-mail" className="textBox" />
-      <input type="text" placeholder="Номер телефона" className="textBox" />
+      <input value={name} onChange={(event) => setName(event.target.value)} type="text" placeholder="Имя" className="textBox" />
+      <input value={email} onChange={(event) => setEmail(event.target.value)} type="text" placeholder="E-mail" className="textBox" />
+      <input value={message} onChange={(event) => setMessage(event.target.value)} type="text" placeholder="Номер телефона" className="textBox" />
       <div className="feedbackButtons">
-        <Button variant="filled" size="md" width="md">Отправить заявку</Button>
+        <Button variant="filled" size="md" width="md" onClick={sendToServerFeedback}>Отправить заявку</Button>
         <ContactButtons />
       </div>
     </form>
